2e6a858b548cee6230539f0d6b9674b4ad55dcc3
alestor123 <alestoraldous@gmail.com>
2021-01-20 14:49:43 +0530

    added cli

--- cli.js
+++ cli.js
@@ -1,1 +1,10 @@
 #!/usr/bin/env node
+var joined = require('./App'),
+chalk = require('chalk'),
+figlet = require('figlet');
+(async () => {
+var date = await joined(process.argv[2] || 'github')
+figlet(' Joined',(err,data) => {
+console.log(chalk.yellowBright(data) + chalk.cyanBright.bold(`\n ${date.name} Joined Github On ${date.joined}`))
+})
+})();
