01e520092281bedb3026147d3e5ba23f089c0230
alestor123 <alestoraldous@gmail.com>
2021-01-20 14:40:12 +0530

    added main code

--- App.js
+++ App.js
@@ -2,4 +2,10 @@ var axios = require('axios'),
 moment = require('moment');
 module.exports = async username => {
 if(!username) throw Error('Username not found')
+var data = await axios.get(`https://api.github.com/users/${username}`)
+return {
+    name:data.data.name,
+    login:data.data.login,
+    joined:moment(data.data.created_at).format("DD MMMM YYYY")
+}
 }
